9691de0038f5e6de3648345007f2e3e2d9953b6e
Andrew Kamarouski <andrew.kamarouski@outlook.com>
2016-04-09 19:54:15 +1000

    Rework blog in jekyll

--- src/kamarouski/index.php
+++
@@ -1,7 +0,0 @@
-<?php get_header(); ?>
-        <div class="posts-latest">
-          <?php while ( have_posts() ) : the_post(); ?>
-      			<?php get_template_part( 'content', '' ); ?>
-      		<?php endwhile; ?>
-        </div>
-<?php get_footer();?>
